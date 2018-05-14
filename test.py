d284f11d7a51d06b4cfa91c2987dcba8e2c1f051
aalind0 <singh.aalind@gmail.com>
2018-05-14 17:24:53 +0800

    Cleanup Done

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import nltk
 import random
 from nltk.classify.scikitlearn import SklearnClassifier
+from sklearn import svm
 import pickle
 
 documents = []
@@ -15,12 +16,14 @@ short_line = open("nus_sms-data.txt","r").read()
     #if i%2 == 0:
         #print(line)
 
-
 for p in short_line.split('\n'):
     if i%2 == 0:
         documents.append((p, "iv"))
+
     else:
-        documents_ovv.append((p, "ovv"))
+        documents.append((p, "ovv"))
     i+=1
 
-print(documents_ovv)
+save_documents = open("documents.pickle", "wb")
+pickle.dump(documents, save_documents)
+save_documents.close()
