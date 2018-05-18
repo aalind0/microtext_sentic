6447ed8669ff5fe926b8ac171fa04e37fb8b9c5a
aalind0 <singh.aalind@gmail.com>
2018-05-18 17:16:22 +0800

    Examples added

--- testing.py
+++ testing.py
@@ -13,3 +13,13 @@ print(vocab.sentiment("Hey wat u doing"))
 print(vocab.sentiment("Was up with u"))
 
 print(vocab.sentiment("I wil txt u latr"))
+
+print(vocab.sentiment("How hav u been?"))
+
+print(vocab.sentiment("Tommm how hv u been."))
+
+print(vocab.sentiment("I was thinking of writing a paper."))
+
+print(vocab.sentiment("Wat time e post office close? I wan to renew my pdl lor..."))
+
+print(vocab.sentiment("lolx i am reading."))
