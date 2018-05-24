c206d930fb57ebf8e9e199d973de8df096c94db4
aalind0 <singh.aalind@gmail.com>
2018-05-24 15:52:22 +0800

    Preprocessed data added

--- pos_tag.py
+++ pos_tag.py
@@ -13,20 +13,21 @@ allowed_word_types = ["J","R","V"]
 
 
 for p in iv.split('\n'):
-    twitter_username_re = re.sub(r'@([A-Za-z0-9_]+)',"", p)
+    twitter_username_re = re.sub(r'@([A-Za-z0-9_]+)','', p)
     twitter_username_re = re.sub(r'http\S+', '', twitter_username_re)
     words = word_tokenize(twitter_username_re)
     pos = nltk.pos_tag(words)
-    print(pos)
+    #print(pos)
 
 
 
 for p in ovv.split('\n'):
-    twitter_username_re = re.sub(r'@([A-Za-z0-9_]+)',"", p)
+    twitter_username_re = re.sub(r'@([A-Za-z0-9_]+)','', p)
     twitter_username_re = re.sub(r'http\S+', '', twitter_username_re)
+    print(twitter_username_re)
     words = word_tokenize(twitter_username_re)
     pos = nltk.pos_tag(words)
-    print(pos)
+    #print(pos)
 
 
 
